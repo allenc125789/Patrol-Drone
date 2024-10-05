1b2145915e27f4bc0b8945c17203fd21aea1a80a
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-10-04 22:42:42 -0500

    Update initBoot.py

--- python3/initBoot.py
+++ python3/initBoot.py
@@ -8,16 +8,67 @@ pTime = 0
 detectorFM = fm.faceDetector()
 detectorPM = pm.poseDetector()
 
-def saveAdminFace(img, bbox, count):
-            adminFolder = "/home/drone/Pictures/Faces/admin/"
-            bboxX = bbox[0][3][0]
-            bboxY = bbox[0][3][1]
-            bboxW = bbox[0][3][2]
-            bboxH = bbox[0][3][3]
-            roi = img[bboxY:bboxY+bboxH, bboxX:bboxX+bboxW]
-            adminPhoto = str(adminFolder) + str(count) + ".jpg"
-            cv2.imwrite(adminPhoto, roi)
 
+def setAdminFace():
+    count = 0
+    WAIT = False
+    while True:
+        #initCam
+        cap.set(cv2.CAP_PROP_FPS,15)
+        success, img = cap.read()
+        centerPOV = img[80: 400,160: 480]
+        imgFace, bboxs = detectorFM.findFaces(img)
+
+        #First boot. Determines admin's face by selecting the most prevelant face.
+        facePose = detectorPM.confirmFacePose(centerPOV)
+        bbox = faceAutoCenter(bboxs)
+        faceCorrectionVal = bbox[0][2][0]
+        faceDetectionVal = bbox[0][1][0]
+        #Keep a multiple of 3 to split evenly between face poses.
+        totalPics = 120
+
+        print(faceCorrectionVal, faceDetectionVal)
+        #If the 'admin's face is in the CenterPOV and has a value of <0.70%, it'll print.
+        if (all(i > 0 for i in faceCorrectionVal) and faceDetectionVal > 0.70):
+            #<= Facing Forward, 1/3 of totalPics
+            if (count <= totalPics / 3 and facePose == "faceForward"):
+                print("Look Forward")
+                print(facePose)
+                detectorFM.saveAdminFace(img, bbox, count)
+                count += 1
+            #<= Facing Left, 2/3 of totalPics
+            elif (count >= totalPics / 3 and count < (totalPics / 3) * 2 and facePose == "faceLeft"):
+                print("Look Left")
+                print(facePose)
+                detectorFM.saveAdminFace(img, bbox, count)
+                count += 1
+            #<= Facing Right, 3/3 of totalPics
+            elif (count >= (totalPics / 3) * 2 and facePose == "faceRight"):
+                print("Look Right")
+                print(facePose)
+                detectorFM.saveAdminFace(img, bbox, count)
+                count += 1
+            #If all pictures taken.
+            if (count == totalPics):
+                break
+        else:
+            continue
+
+
+
+#Determines pixel difference between faces and the centerPOV.
+def faceAutoCenter(bboxs):
+    try:
+        output = []
+        for i in bboxs:
+            faceID = i[0]
+            bboxSize = i[1]
+            faceDetectionVal = i[2]
+            faceCorrectionVal = bboxCenterTracking(bboxSize) # and faceVal[0] > 0.65):
+            output.append([faceID, faceDetectionVal, faceCorrectionVal, bboxSize])
+        return output
+    except:
+        return "null"
 
 #Converts location of bbox into difference between centerPOV.
 # `faceCorrectionVal` values are >= 0, if inside centerPOV.
@@ -34,49 +85,13 @@ def bboxCenterTracking(bbox):
         return "null"
 
 
+
+#Main loop
 while True:
     #initCam
-    cap.set(cv2.CAP_PROP_FPS,15)
-    success, img = cap.read()
-    centerPOV = img[80: 400,160: 480]
-    imgFace, bboxs = detectorFM.findFaces(img)
+    setAdminFace()
 
-    #First boot. Determines admin's face by selecting the most prevelant face.
-    facePose = detectorPM.confirmFacePose(centerPOV)
-    bbox = detectorFM.faceAutoCenter(bboxs)
-    faceCorrectionVal = bbox[0][2][0]
-    faceDetectionVal = bbox[0][1][0]
-    #Keep a multiple of 3 to split evenly between face poses.
-    totalPics = 120
-    count = 0
-
-    #If the 'admin's face is in the CenterPOV and has a value of <0.70%, it'll initiate...something...
-    print(count)
-    if (count <= totalPics / 3 and facePose == "faceForward"):
-        print("Look Forward")
-        print(facePose)
-        saveAdminFace(img, bbox, count)
-        count += 1
-    elif (all(i < 0 for i in faceCorrectionVal) and faceDetectionVal < 0.70):
-        continue
-    elif (count >= totalPics / 3 and count < (totalPics / 3) * 2 and facePose == "faceLeft"):
-        print("Look Left")
-        print(facePose)
-        saveAdminFace(img, bbox, count)
-        count += 1
-    elif (count >= (totalPics / 3) * 2 and facePose == "faceRight"):
-        print("Look Right")
-        print(facePose)
-        saveAdminFace(img, bbox, count)
-        count += 1
-    elif facePose == False:
-        print("rip")
-    if (count == totalPics):
-        break
     #End of loop
-    key = cv2.waitKey(30)
-    #Press ESC key to exit.
-    if key == 27:
-        cap.release()
-        cv2.destroyAllWindows()
-        break
+    cap.release()
+    cv2.destroyAllWindows()
+    break
