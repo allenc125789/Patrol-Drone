73260e77793b93edc8e7804c336e20db4220f5ce
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-10-06 00:43:22 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -45,3 +45,4 @@ chown -R drone:drone "/home/drone"
 #: Python3 Settings
 sudo -H -u drone bash -c 'cd "/home/drone" && python3 -m venv ".venv"'
 sudo -H -u drone bash -c '/home/drone/.venv/bin/pip3 install mediapipe mariadb tensorflow face-recognition --use-pep517'
+sudo reboot
