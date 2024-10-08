2a534dc9e0721c529f63797f881d2432210bd6d2
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-10-08 00:19:31 -0500

    Update faceRecModules.py

--- python3/faceRecModules.py
+++ python3/faceRecModules.py
@@ -3,12 +3,14 @@ import os
 import cv2
 
 KNOWN_FACES_DIR ="/home/drone/Pictures/Faces/catologued"
-UNKNOWN_FACES_DIR ="/home/drone/Pictures/Faces/uncatologued"
+#UNKNOWN_FACES_DIR ="/home/drone/Pictures/Faces/uncatologued"
 TOLERANCE = 0.6
 
 FRAME_THICKNESS = 3
 FONT_THICKNESS = 2
 MODEL = "hog"
+
+cap = cv2.VideoCapture(0)
 
 print("loading known faces")
 
@@ -32,14 +34,16 @@ for name in os.listdir(KNOWN_FACES_DIR):
 
 
 print("process uknown faces")
-for filename in os.listdir(UNKNOWN_FACES_DIR):
-    print(filename)
-    image = face_recognition.load_image_file(f"{UNKNOWN_FACES_DIR}/{filename}")
-    locations = face_recognition.face_locations(image, model=MODEL)
-    encodings = face_recognition.face_encodings(image, locations)
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
+while True:
     try:
+#    print(filename)
+#        image = face_recognition.load_image_file(f"{UNKNOWN_FACES_DIR}/{filename}")
+        cap.set(cv2.CAP_PROP_FPS,15)
+        ret, video = cap.read()
+        image = cv2.cvtColor(video, cv2.COLOR_BGR2RGB)
+        locations = face_recognition.face_locations(image, model=MODEL)
+        encodings = face_recognition.face_encodings(image, locations)
+
         for face_encoding, face_location in zip(encodings, locations):
             results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE)
             match = None
@@ -55,7 +59,11 @@ for filename in os.listdir(UNKNOWN_FACES_DIR):
                 bottom_right = (face_location[1], face_location[2]+22)
                 cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
                 cv2.putText(image, match, (face_location[3]+10, face_location[2]+15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200,200,200), FONT_THICKNESS)
-        cv2.imshow(filename, image)
-        cv2.waitKey(10000)
-    except Exception as e: print(e)
-#    cv2.destroyAllWindows(filename)
+        cv2.imshow("cap", image)
+        key = cv2.waitKey(30)
+        if key == 27:
+            cv2.destroyAllWindows()
+            break
+    except Exception as e:
+        print(e)
+#        break
