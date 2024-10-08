5c98af51fb173edff72a5cfa793143bf414657e1
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-10-08 04:20:02 -0500

    Update faceRecModules.py

--- python3/faceRecModules.py
+++ python3/faceRecModules.py
@@ -3,7 +3,7 @@ import os
 import cv2
 
 KNOWN_FACES_DIR ="/home/drone/Pictures/Faces/catologued"
-#UNKNOWN_FACES_DIR ="/home/drone/Pictures/Faces/uncatologued"
+UNKNOWN_FACES_DIR ="/home/drone/Pictures/Faces/uncatologued"
 TOLERANCE = 0.6
 
 FRAME_THICKNESS = 3
@@ -34,13 +34,13 @@ for name in os.listdir(KNOWN_FACES_DIR):
 
 
 print("process uknown faces")
-while True:
+for filename in os.listdir(UNKNOWN_FACES_DIR):
     try:
-#    print(filename)
-#        image = face_recognition.load_image_file(f"{UNKNOWN_FACES_DIR}/{filename}")
-        cap.set(cv2.CAP_PROP_FPS,15)
-        ret, video = cap.read()
-        image = cv2.cvtColor(video, cv2.COLOR_BGR2RGB)
+        print(filename)
+        imageBGR = face_recognition.load_image_file(f"{UNKNOWN_FACES_DIR}/{filename}")
+#        cap.set(cv2.CAP_PROP_FPS,15)
+#        ret, video = cap.read()
+        image = cv2.cvtColor(imageBGR, cv2.COLOR_BGR2RGB)
         locations = face_recognition.face_locations(image, model=MODEL)
         encodings = face_recognition.face_encodings(image, locations)
 
@@ -50,20 +50,17 @@ while True:
             if True in results:
                 match = known_names[results.index(True)]
                 print(f"Match found: {match}")
-                top_left = (face_location[3], face_location[0])
-                bottom_right = (face_location[1], face_location[2])
-                color = [0, 255, 0]
-                cv2.rectangle(image, top_left, bottom_right, color, FRAME_THICKNESS)
+#                top_left = (face_location[3], face_location[0])
+#                bottom_right = (face_location[1], face_location[2])
+#                color = [0, 255, 0]
+#                cv2.rectangle(image, top_left, bottom_right, color, FRAME_THICKNESS)
 
-                top_left = (face_location[3], face_location[2])
-                bottom_right = (face_location[1], face_location[2]+22)
-                cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
-                cv2.putText(image, match, (face_location[3]+10, face_location[2]+15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200,200,200), FONT_THICKNESS)
-        cv2.imshow("cap", image)
-        key = cv2.waitKey(30)
-        if key == 27:
-            cv2.destroyAllWindows()
-            break
+#                top_left = (face_location[3], face_location[2])
+#                bottom_right = (face_location[1], face_location[2]+22)
+#                cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
+#                cv2.putText(image, match, (face_location[3]+10, face_location[2]+15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200,200,200), FONT_THICKNESS)
+#        cv2.imshow("cap", image)
+#        cv2.waitKey(10000)
     except Exception as e:
         print(e)
 #        break
