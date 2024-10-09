e6a30d7553550af0deeb70323ae5a3d4373b9ccc
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-10-08 23:59:52 -0500

    Update faceRecModules.py

--- python3/faceRecModules.py
+++ python3/faceRecModules.py
@@ -2,24 +2,28 @@ import face_recognition
 import os
 import cv2
 
+
+
+KNOWN_FACES_DIR ="/home/drone/Pictures/Faces/catologued"
+UNKNOWN_FACES_DIR ="/home/drone/Pictures/Faces/uncatologued"
+TOLERANCE = 0.6
+
+MODEL = "hog"
+
+known_faces = []
+known_names = []
+
+
 class faceRec():
 
-    KNOWN_FACES_DIR ="/home/drone/Pictures/Faces/catologued"
-    UNKNOWN_FACES_DIR ="/home/drone/Pictures/Faces/uncatologued"
-    TOLERANCE = 0.6
-    
-    MODEL = "hog"
-        
-    known_faces = []
-    known_names = []
 
-    def createFaceModel():
+    def createFaceModel(self):
         #Loading Known Faces
         for name in os.listdir(KNOWN_FACES_DIR):
             for filename in os.listdir(f"{KNOWN_FACES_DIR}/{name}"):
                 image = face_recognition.load_image_file(f"{KNOWN_FACES_DIR}/{name}/{filename}")
                 temp_encoding = face_recognition.face_encodings(image)
-        
+
                 if len(temp_encoding) > 0 :
                     encoding = temp_encoding[0]
                 else:
@@ -30,23 +34,23 @@ class faceRec():
         return known_faces, known_names
 
 
-    def analyzeNewFaces():
+    def analyzeNewFaces(self):
         #Loading Unknown Faces
         for filename in os.listdir(UNKNOWN_FACES_DIR):
             try:
                 print(filename)
                 imageBGR = face_recognition.load_image_file(f"{UNKNOWN_FACES_DIR}/{filename}")
-        
+
                 image = cv2.cvtColor(imageBGR, cv2.COLOR_BGR2RGB)
                 locations = face_recognition.face_locations(image, model=MODEL)
                 encodings = face_recognition.face_encodings(image, locations)
-        
+
                 for face_encoding, face_location in zip(encodings, locations):
                     results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE)
                     match = None
                     if True in results:
                         match = known_names[results.index(True)]
                         print(f"Match found: {match}")
-        
+
             except Exception as e:
                 print(e)
