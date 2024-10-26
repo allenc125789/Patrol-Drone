efc8ba5bdd05455e82eccd9eb08c2c9502e28bc7
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-10-25 22:17:50 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@ vFILESYSTEM=$(df -P . | sed -n '$s/[[:blank:]].*//p')
 vUUID=$(/usr/sbin/blkid -s UUID -o value "$vFILESYSTEM")
 
 #: Dependancies.
+apt-get update
 aDEPENDS=("sudo" "network-manager" "cmake" "libmariadb3" "mariadb-server" "build-essential" "libssl-dev" "libffi-dev" "python3-dev" "libmariadb-dev" "python3.11-venv" "python3-opencv" "python3-matplotlib" "python3-numpy" "xfce4" "xfonts-base")
     #: Dependancy Check
 apt-get install ${aDEPENDS[*]}
