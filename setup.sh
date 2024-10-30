ac6cf1dc87bb8729c39d70b7987c29390eaa6fee
allenc125789 <67094410+allenc125789@users.noreply.github.com>
2024-10-30 18:07:44 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -24,6 +24,8 @@ fi
 sudo useradd -m drone
 echo drone:1234 | sudo chpasswd
 sudo usermod -aG sudo drone
+sudo usermod -aG video drone
+
 
 #: Create Files and Folders
 mkdir -v -p "/home/drone/Pictures/Faces/catologued/admin"
